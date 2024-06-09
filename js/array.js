c370cfcb6f4f23a334327a937aae59b06d3d2eb5
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-06-09 13:23:51 +0600

    good work

--- js/array.js
+++ js/array.js
@@ -1,5 +1,9 @@
 const mango = [3, 5, 6, 7, 7, 8, 33];
 console.log(mango.indexOf(33));
 
-const frind = ["mountain", "dew", "eting", "cow"];
-console.log(frind.indexOf("dew"));
+const frind = ["mountain", "dew", "eting", "cow", "got"];
+console.log(frind[4]);
+
+const number = [44, 565, 76, 22, 88, 09, 24];
+const outElement = number[3];
+console.log(outElement);
