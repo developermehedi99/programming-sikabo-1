531e60b865f66940e85c2b1b8eadf36df5ab0d02
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-06-10 12:41:08 +0600

    array done

--- js/array.js
+++ js/array.js
@@ -5,9 +5,12 @@ const frind = ["mountain", "dew", "eting", "cow", "got"];
 // console.log(frind[4]);
 
 const number = [44, 565, 76, 22, 88, 09, 24];
-const secondele = number[2];
+// const secondele = number[2];
 
 // console.log(secondele);
+number.sort();
+
+// console.log(number);
 
 const fish = ["elis", "sing", "katol", "rui"];
 // const extrafist = fish[1];
@@ -22,6 +25,13 @@ fish.push("silver", "puti");
 const sonka = [33, 5, 6, 7, 8, 22, 66];
 // sonka.push(11, 99);
 // sonka.pop();
-sonka.splice(2, 3);
+// sonka.splice(2, 3);
 
-console.log(sonka);
+// sonka.unshift(77,88);
+sonka.shift();
+
+// console.log(sonka);
+
+const price = [2, 5, 7, 1, 3, 9, 8];
+price.sort();
+console.log(price);
