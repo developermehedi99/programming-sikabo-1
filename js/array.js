6fdb644bec8d5cf61aaedc2366a3fe5acd4dd375
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-06-10 12:19:30 +0600

    practise

--- js/array.js
+++ js/array.js
@@ -1,9 +1,27 @@
 const mango = [3, 5, 6, 7, 7, 8, 33];
-console.log(mango.indexOf(33));
+// console.log(mango.indexOf(33));
 
 const frind = ["mountain", "dew", "eting", "cow", "got"];
-console.log(frind[4]);
+// console.log(frind[4]);
 
 const number = [44, 565, 76, 22, 88, 09, 24];
-const outElement = number[3];
-console.log(outElement);
+const secondele = number[2];
+
+// console.log(secondele);
+
+const fish = ["elis", "sing", "katol", "rui"];
+// const extrafist = fish[1];
+
+fish.push("silver", "puti");
+
+// console.log(fish);
+
+// console.log(fish.lastIndexOf("katol"));
+// console.log(fish.length);
+
+const sonka = [33, 5, 6, 7, 8, 22, 66];
+// sonka.push(11, 99);
+// sonka.pop();
+sonka.splice(2, 3);
+
+console.log(sonka);
